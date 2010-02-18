f9546329a05f1ae39542f78d57b3dbf05dba94d8
pair-programmer <chris@inquus.com>
2010-02-17 20:20:08 -0500

    Upgrade to sbt 0.7.0.

--- project/build/SbtGrowlTestsProject.scala
+++ project/build/SbtGrowlTestsProject.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 class TestPluginProject(info:ProjectInfo) extends PluginProject(info) {
+  override def disableCrossPaths = true
+
   val junit = "junit" % "junit" % "4.5" % "test"
   val specs = "org.scala-tools.testing" % "specs" % "1.6.1" % "test"
 
